9463fe7e848c53f3fffb99008c6a7891ce774769
naptestdev <naptestfordev@gmail.com>
2023-10-20 21:21:21 +0700

    feat: add action tooltips to search cards

--- scripts/search.js
+++ scripts/search.js
@@ -23,19 +23,32 @@ const filtered = products.filter(({ name }) => {
 
 for (let product of filtered) {
   let card = /*html*/ `
-  <a href="./product.html?id=${product.id}" class="card">
-    <div class="image-container">
-      <img src="./assets/products/${product.image}" /> 
-    </div> 
-    <div class="container">
-      <p>${
-        categories.find((category) => product.category === category.id).name
-      }</p>
-      <h5>${product.name}</h5> 
-      <div class="stars"><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-regular fa-star"></i></div>
-      <h6><span>${product.oldPrice}</span> <span>${product.newPrice}</span></h6>
+  <div onclick="location.href='./product.html?id=${product.id}'" class="card">
+  <div class="image-container">
+    <img src="./assets/products/${product.image}" /> 
+    <div class="card-actions">
+      <button onclick="event.stopPropagation();addToCart('${
+        product.id
+      }');alert('Added to cart')" data-tooltip="Add to cart">
+        <i class="fa-solid fa-cart-shopping"></i>
+      </button>
+      <button onclick="event.stopPropagation();location.href='./product.html?id=${
+        product.id
+      }'" data-tooltip="Quick view"><i class="fa-regular fa-eye"></i></button>
+      <button onclick="event.stopPropagation();addToWishlist('${
+        product.id
+      }');alert('Added to wishlist')" data-tooltip="Add to wishlist"><i class="fa-regular fa-heart"></i></button>
     </div>
-  </a>
+  </div> 
+  <div class="container">
+    <p>${
+      categories.find((category) => product.category === category.id).name
+    }</p>
+    <h5>${product.name}</h5> 
+    <div class="stars"><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-regular fa-star"></i></div>
+    <h6><span>${product.oldPrice}</span> <span>${product.newPrice}</span></h6>
+  </div>
+</div>
   `;
 
   document.getElementById("products").innerHTML += card;
